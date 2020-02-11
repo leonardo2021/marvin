4a4a4be9e1d0be393d84a51cccf322486962c5d5
leonardo2021 <59874287+leonardo2021@users.noreply.github.com>
2020-02-11 11:19:18 -0300

    marvin

---
+++ fase04/ex02/fizzbuzz.js
@@ -0,0 +1,21 @@
+  
+function fizzbuzz(num1, num2){
+    
+    while (num1 <= num2){
+        
+        if (num1 %3 === 0 && num1 % 5 === 0){
+            console.log(num1 + " Fizzbuzz");
+            num1++
+        }
+        if (num1 % 3 === 0){
+            console.log(num1 + " Fizz");
+            num1++
+        }
+        if (num1 %5 === 0){
+            console.log(num1 + " Buzz");
+            num1++
+        }
+        console.log(num1)
+        num1++
+    }
+}
